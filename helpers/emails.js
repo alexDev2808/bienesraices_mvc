11bb23639e6c98f29bdd7706da077fc713cafb39
alexDev2808 <colores.2808.8000@gmail.com>
2023-12-20 18:32:50 -0600

    Probando el envio de emails

--- helpers/emails.js
+++ helpers/emails.js
@@ -9,6 +9,24 @@ const emailRegistro = async (datos) => {
           pass: process.env.EMAIL_PASS
         }
       });
+
+      const { email, nombre, token } = datos
+
+    //   enviar email
+    await transport.sendMail({
+        from: 'admin@bienesraices.com',
+        to: email,
+        subject: 'Confirma tu cuenta en BienesRaices.com',
+        text: 'Confirma tu cuenta en BienesRaices.com',
+        html: `
+            <p>Hola ${nombre}, comprueba tu cuenta en bienesraices.com</p>
+
+            <p>Tu cuenta ya esta lista, solo debes confirmarla en el siguiente enlace:
+            <a href="">Confirmar cuenta</a></p>
+
+            <p>Si tu no creaste tu cuenta, puedes ignorar este mensaje</p>
+        `
+    })
 }
 
 
