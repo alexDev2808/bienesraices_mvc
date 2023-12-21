714aba14ba931369dfff13d3abfdaab96d1567c0
alexDev2808 <colores.2808.8000@gmail.com>
2023-12-21 12:07:48 -0600

    Confirmando la cuenta de usuario

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -97,6 +97,17 @@ const confirmar = async (req, res) => {
     }
 
     // Confirmar la cuenta
+    usuario.token = null;
+    usuario.confirmado = true;
+
+    await usuario.save();
+
+    res.render('auth/confirmar-cuenta', {
+        pagina: 'Cuenta confirmada',
+        mensaje: 'La cuenta se confirmo correctamente.'
+    })
+
+
 }
 
 
