4f21079b34ec7ba297f7c9c1df15ebdb07bdd15a
alexDev2808 <colores.2808.8000@gmail.com>
2023-12-23 08:49:25 -0600

    Almacenando Calle, long y lat

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -29,6 +29,11 @@
         geocodeService.reverse().latlng(posicion, 13).run(function(error, resultado){
             // console.log(resultado);
             marker.bindPopup(resultado.address.LongLabel)
+
+            document.querySelector('.calle').textContent = resultado?.address?.Address ?? '';
+            document.querySelector('#calle').value = resultado?.address?.Address ?? '';
+            document.querySelector('#lat').value = resultado?.latlng?.lat ?? '';
+            document.querySelector('#lng').value = resultado?.latlng?.lng ?? '';
         })
     })
 
