f2be320e1cf1d4a74fc08d27d8aab086e0ade87e
alexDev2808 <colores.2808.8000@gmail.com>
2023-12-22 21:17:27 -0600

    Mostrar mapa

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -1,1 +1,12 @@
-alert('Soy el mapa')
+(function() {
+    const lat = 19.31623;
+    const lng = -98.2381746;
+    const mapa = L.map('mapa').setView([lat, lng ], 16);
+    
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mapa);
+
+
+})()
