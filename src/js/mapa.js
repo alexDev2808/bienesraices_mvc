c7036a7d32b0d71f3bd581d2ec4c04bc61ed137e
alexDev2808 <colores.2808.8000@gmail.com>
2023-12-22 21:34:09 -0600

    Agregando pin con la ubicacion

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -2,11 +2,18 @@
     const lat = 19.31623;
     const lng = -98.2381746;
     const mapa = L.map('mapa').setView([lat, lng ], 16);
+    let marker;
     
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(mapa);
 
+    // Pin
+    marker = new L.marker([lat, lng], {
+        draggable: true,
+        autoPan: true
+    })
+    .addTo(mapa)
 
 })()
